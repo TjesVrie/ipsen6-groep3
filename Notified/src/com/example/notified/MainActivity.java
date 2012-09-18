e86f5ab314a2357e39ab6d86edafae7529bc040c
Tjesco de Vries <tjesco.com@gmail.com>
2012-09-19 00:46:51 +0200

    Klasse GitVragen toegevoegd en aanpassingen aan MainActivity - niet voor het eggie!

--- Notified/src/com/example/notified/MainActivity.java
+++ Notified/src/com/example/notified/MainActivity.java
@@ -6,25 +6,33 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-        
-        //Hoi, ik zit naast Alister.
-    }
-    
-    /*
-     * Simple method by Tjesco, doing fuck all
-     */
-    public void doStuff()
-    {
-    	// do stuff
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+
+		// Hoi, ik zit naast Alister.
+	}
+
+	/*
+	 * Simple method by Tjesco, doing everything you need
+	 */
+	public void doStuff() {		
+		System.out.println(ask(GitVragen.getVraag()));
+	}
+
+	/*
+	 * Get your answers here!
+	 */
+	private String ask(String question) {
+		int i = question.length();
+		int answer = new java.util.Random().nextInt(i);
+		return ("Antwoord #" + answer);
+	}
 }
