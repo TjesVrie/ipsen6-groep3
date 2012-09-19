29f9d99d6d3debd6857e7ec5d67bfd37fe2de211
Tjesco de Vries <tjesco.com@gmail.com>
2012-09-19 16:05:19 +0200

    emptied MainActivity and deleted unused classes

--- Notified/src/com/example/notified/MainActivity.java
+++ Notified/src/com/example/notified/MainActivity.java
@@ -6,34 +6,5 @@ import android.view.Menu;
 
 public class MainActivity extends Activity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-
-		// Hoi, ik zit naast Alister.
-	}
-
-	/*
-	 * Simple method by Tjesco, doing everything you need
-	 */
-	public void doStuff() {		
-		System.out.println(ask(GitVragen.getVraag()));
-	}
-
-	/*
-	 * Get your answers here!
-	 */
-	private String ask(String question) {
-		int i = question.length();
-		int answer = new java.util.Random().nextInt(i);
-		answer++;
-		return ("Antwoord #" + answer);
-	}
 }
