9154f4a7baf8dcefb73f7c5eed1b060e412e79fc
Alister Jones <alister.jones@hotmail.com>
2012-09-27 11:53:45 +0200

    infodingen

---
+++ Notified/src/com/example/notified/InfoMededeling.java
@@ -0,0 +1,14 @@
+package com.example.notified;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoMededeling extends Activity 
+{
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.infomededeling);
+	}
+}
